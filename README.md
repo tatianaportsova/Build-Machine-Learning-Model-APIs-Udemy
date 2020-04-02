8843ebf8cb9580f44e179369001ad793488f67eb
tatianaportsova <tt.sova@gmail.com>
2020-04-01 21:35:50 -0400

    Section 6.5 - Data Validation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Build-Machine-Learning-Model-APIs-Udemy
 For the documentation, visit the course on Udemy.
+
+''' pip install -r requirements.txt
+''' pytest tests -W ignore::DeprecationWarning
--- regression_model/config/config.py
+++ regression_model/config/config.py
@@ -1,11 +1,19 @@
+import pathlib
+
+import regression_model
+
+
+PACKAGE_ROOT = pathlib.Path(regression_model.__file__).resolve().parent
+TRAINED_MODEL_DIR = PACKAGE_ROOT / 'trained_models'
+DATASET_DIR = PACKAGE_ROOT / 'datasets'
 
 # data
-TRAINING_DATA_FILE = "houseprice.csv"
-PIPELINE_NAME = 'lasso_regression'
-
+TESTING_DATA_FILE = 'test.csv'
+TRAINING_DATA_FILE = 'train.csv'
 TARGET = 'SalePrice'
 
-# input variables 
+
+# variables
 FEATURES = ['MSSubClass', 'MSZoning', 'Neighborhood',
             'OverallQual', 'OverallCond', 'YearRemodAdd',
             'RoofStyle', 'MasVnrType', 'BsmtQual', 'BsmtExposure',
@@ -17,7 +25,7 @@ FEATURES = ['MSSubClass', 'MSZoning', 'Neighborhood',
             'YrSold']
 
 # this variable is to calculate the temporal variable,
-# must be dropped afterwards
+# can be dropped afterwards
 DROP_FEATURES = 'YrSold'
 
 # numerical variables with NA in train set
@@ -37,3 +45,13 @@ CATEGORICAL_VARS = ['MSZoning', 'Neighborhood', 'RoofStyle', 'MasVnrType',
                     'BsmtQual', 'BsmtExposure', 'HeatingQC', 'CentralAir',
                     'KitchenQual', 'FireplaceQu', 'GarageType', 'GarageFinish',
                     'PavedDrive']
+
+NUMERICAL_NA_NOT_ALLOWED = [
+    feature for feature in FEATURES
+    if feature not in CATEGORICAL_VARS + NUMERICAL_VARS_WITH_NA
+]
+
+CATEGORICAL_NA_NOT_ALLOWED = [
+    feature for feature in CATEGORICAL_VARS
+    if feature not in CATEGORICAL_VARS_WITH_NA
+]
